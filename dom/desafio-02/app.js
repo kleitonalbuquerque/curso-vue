82a54679556d4a734afd51ff9e5cefd56b791985
kleitonalbuquerque <kleiton2102@gmail.com>
2019-05-13 00:54:01 -0300

    Desafio 2, interpolação, propriedades reativas e sintaxe reduzida

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
-    }
+    },
+    methods: {
+        exibirAlerta() {
+            alert('Curso Vue')
+        },
+        alterarValor(event) {
+            this.valor = event.target.value
+        }
+    },
 })
