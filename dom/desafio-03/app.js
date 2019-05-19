8f4c0216018cf9302fe262dd7bae1eb2e4e5c16d
kleitonalbuquerque <kleiton2102@gmail.com>
2019-05-19 12:08:07 -0300

    Estilo Dinâmico e Classe CSS #01

--- dom/desafio-03/app.js
+++ dom/desafio-03/app.js
@@ -2,5 +2,30 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0
-    }
+    },
+    computed: {
+        resultado() {
+           return this.valor == 37 ? 'Valor igual' : 'Valor Diferente'
+         }
+    },
+    watch: {
+        valor() {
+            if (this.valor >= 37) {
+                setTimeout(() => {
+                    this.valor = 0
+                }, 5000)
+            }
+        }
+    },
+    // methods: {
+    // aumentaUm() {
+    //     this.valor++
+    // }, 
+    // aumentaCinco() {
+    //     this.valor += 5
+    // },
+    // resultado() {
+    //     this.valor < 37 ? 'Valor Diferente' : 'Valor igual'
+    //   }  
+    // },
 });
