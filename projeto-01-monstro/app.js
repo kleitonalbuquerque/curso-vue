ae6e87e6a9fd5b51ed2029038e90b7e596f9f617
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-16 16:16:30 -0300

    Implementando Atacar

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -15,9 +15,25 @@ new Vue({
 			this.running = true
 			this.playerLife = 100
 			this.monsterLife = 100
+		},
+		attack(especial) {
+			// console.log(especial, this.getRandom(5, 10))
+			this.hurt('playerLife', 7, 12, false)
+			this.hurt('monsterLife', 5, 10, especial)
+		},
+		hurt(atr, min, max, especial) {
+			const plus = especial ? 5 : 0
+			const hurt = this.getRandom(min + plus, max + plus)
+			this[atr] = Math.max(this[atr] - hurt, 0)
+		},
+		getRandom(min, max) {
+			const value = Math.random() * (max - min) + min
+			return Math.round(value)
 		}
 	},
 	watch: {
-
+		hasResult(value) {
+			if (value) this.running = false
+		}
 	}
 })
