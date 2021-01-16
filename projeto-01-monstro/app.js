3f5d2065b4da454c7af7614dee5570e21b9bb2cc
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-16 15:46:29 -0300

    Mostrando o Resultado

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,11 +1,13 @@
 new Vue({
 	el: '#app',
 	data: {
-		playerLife: 100,
-		monsterLife: 100,
+		playerLife: 0,
+		monsterLife: 30,
 	},
 	computed: {
-
+		hasResult() {
+			return this.playerLife == 0 || this.monsterLife == 0
+		}
 	},
 	methods: {
 
