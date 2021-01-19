64cee8cfc886c9399a088dce068aaebe4d9a5c22
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-19 18:08:41 -0300

    Validando props

--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -10,7 +10,16 @@
 
 <script>
 export default {
-  props: ['nome'],
+  props: {
+    nome: {
+      type: String,
+      // required: true
+      // default: 'Anônimo',
+      default: function() {
+        return Array(10).fill(0).join(',')
+      }
+    }
+  },
   methods: {
     inverterNome() {
       return this.nome.split('').reverse().join('')
