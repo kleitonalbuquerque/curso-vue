0d4ccfab5dd371f394fb1d11aa73564c50c463da
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-21 15:03:24 -0300

    Passando Conteúdo com Slots

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="citacao">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
