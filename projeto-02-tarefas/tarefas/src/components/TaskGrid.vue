0659631e8046acaffd98ddb3208860643d896166
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-22 10:27:23 -0300

    Componente Lista de Tarefas

--- projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
+++ projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="task-grid">
-    <Task v-for="task in tasks" :key="task.name" :task="task"> 
-      {{ task.name }}
-    </Task>
+  <div class="task-grid" v>
+    <template v-if="tasks.length">
+      <Task v-for="task in tasks" :key="task.name" :task="task">
+        {{ task.name }}
+      </Task>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
   </div>
 </template>
 
 <script>
-import Task from './Task.vue'
+import Task from "./Task.vue";
 export default {
   props: {
-    tasks: { type: Array, required: true }
+    tasks: { type: Array, required: true },
   },
   components: {
-    Task
-  }
-}
+    Task,
+  },
+};
 </script>
 
 <style>
-  
+  .task-grid {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap; /* quebrar linha no flex-box */
+  }
+
+  .task-grid .task {
+    margin: 10px;
+  }
+
+  .no-task {
+    color: #AAA;
+    font-size: 1.7rem;
+  }
 </style>
