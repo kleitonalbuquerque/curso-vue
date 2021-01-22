46bca9da516689bc06421c93dfed29fd1e5b8b8f
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-22 10:00:27 -0300

    Setup projeto Tarefas e criação dos componentes: Tasks e TaskGrid

---
+++ projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="task-grid">
+    <Task v-for="task in tasks" :key="task.name" :task="task"> 
+      {{ task.name }}
+    </Task>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+  props: {
+    tasks: { type: Array, required: true }
+  },
+  components: {
+    Task
+  }
+}
+</script>
+
+<style>
+  
+</style>
