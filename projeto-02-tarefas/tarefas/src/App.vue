e29c1950494ed03fe5f380e2e5ca50770434dd1e
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-22 14:04:06 -0300

    Usando Local Storage

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -34,6 +34,14 @@ export default {
 			return Math.round(done / total * 100) || 0
 		}
 	},
+	watch: {
+		tasks: {
+			deep: true,
+			handler() {
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
 	methods: {
 		addTask(task) {
 			const sameName = t => t.name === task.name
@@ -51,6 +59,11 @@ export default {
 		toggleTaskState(i) {
 			this.tasks[i].pending = !this.tasks[i].pending
 		}
+	},
+	created() {
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		this.tasks =  Array.isArray(array) ? array : []
 	},
 }
 </script>
