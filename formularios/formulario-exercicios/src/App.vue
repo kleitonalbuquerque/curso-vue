26c999a6ab43bb78978ff87ea5b3b74271961130
kleitonalbuquerque <kleiton2102@gmail.com>
2021-01-25 20:42:19 -0300

    Usando <textarea> e Salvando Quebras de Linha

--- formularios/formulario-exercicios/src/App.vue
+++ formularios/formulario-exercicios/src/App.vue
@@ -14,7 +14,7 @@
 					<input type="number" v-model.number="usuario.idade">
 				</Rotulo>
 				<Rotulo nome="Mensagem">
-					<textarea name="" cols="30" rows="5"></textarea>
+					<textarea name="" cols="30" rows="5" v-model="mensagem"></textarea>
 				</Rotulo>
 				<Rotulo nome="Características do Problema">
 					<span class="mr-4"><input type="checkbox" value="reproduzivel"> Reproduzível</span>
@@ -48,7 +48,7 @@
 					<span> {{ usuario.idade }} {{ tipoIdade }} </span>
 				</Rotulo>
 				<Rotulo nome="Mensagem">
-					<span>???</span>
+					<span style="white-space: pre;"> {{ mensagem }} </span>
 				</Rotulo>
 				<Rotulo nome="Marque as Opções">
 					<span>???</span>
@@ -81,6 +81,7 @@ export default {
 	},
 	data() {
 		return {
+			mensagem: '',
 			usuario: {
 				email: '',
 				senha: '',
